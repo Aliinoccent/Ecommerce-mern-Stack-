bd91c4c93839bb50f5ebe49c8fd4bab143453bed
unknown <aliinoccent786@gmail.com>
2024-10-30 12:49:49 -0700

    update admin panal

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -7,7 +7,7 @@ const upload = require('../middelwares/melter.js');
 const {catgory,getallproductsByCategory,productAdd,getallproducts,getPriceOfProducts,getCategory}=controller
 router.post('/',upload.single('image'),catgory);
 router.get('/categorData',getCategory);
-router.post('/:categoryid',upload.single('image'),productAdd)
+router.post('/:categoryid',productAdd)
 router.get('/catgoryid=:catgoryid',getallproductsByCategory)
 router.get('/', getallproducts);
 router.get('/price=:price',getPriceOfProducts)
